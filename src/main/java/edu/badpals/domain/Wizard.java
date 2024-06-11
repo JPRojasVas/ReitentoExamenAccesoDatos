932dfc37ffc611736535ed4b5c3d63cad6225bb0
JPRojasVas <Juanparovas3@gmail.com>
2024-06-11 23:06:24 +0200

    feat (Wizard.java) : mapping to database

--- src/main/java/edu/badpals/domain/Wizard.java
+++ src/main/java/edu/badpals/domain/Wizard.java
@@ -1,7 +1,64 @@
 package edu.badpals.domain;
 
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "t_wizards")
+public class Wizard extends PanacheEntityBase {
 
-public class Wizard {
+    @Id
+    @Column(name = "wizard_name")
+    private String name;
+
+    @Column(name = "wizard_dexterity")
+    private int dexterity;
+
+    @Column(name = "wizard_person")
+    @Enumerated(EnumType.STRING)
+    private WizardPerson person;
+
+    public Wizard(){
+
+    }
+
+    public Wizard(String name, int dexterity, WizardPerson person) {
+        this.name = name;
+        this.dexterity = dexterity;
+        this.person = person;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public WizardPerson getPerson() {
+        return person;
+    }
+
+    public void setPerson(WizardPerson person) {
+        this.person = person;
+    }
+
+    @Override
+    public String toString() {
+        return "Wizard{" +
+                "name='" + name + '\'' +
+                ", dexterity=" + dexterity +
+                ", person=" + person +
+                '}';
+    }
 }
