95bb615933e5f4e615d16143d6fb7c6f2f6bb488
JPRojasVas <Juanparovas3@gmail.com>
2024-06-11 22:52:56 +0200

    feat (Wizard.java) : Create class

---
+++ src/main/java/edu/badpals/domain/Wizard.java
@@ -0,0 +1,7 @@
+package edu.badpals.domain;
+
+
+
+
+public class Wizard {
+}
--- src/test/java/edu/badpals/domain/MagicalItemIT.java
+++
@@ -1,8 +0,0 @@
-package edu.badpals.domain;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class MagicalItemIT extends MagicalItemTest {
-    // Execute the same tests but in packaged mode.
-}
