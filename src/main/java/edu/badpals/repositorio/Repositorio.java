05cbd6affd6b6e6c3559479d6a4dfef2979685b4
a23juanprv <Juanparovas3@gmail.com>
2024-06-12 10:36:28 +0200

    feat (Repositorio.java) : add method loadItem

--- src/main/java/edu/badpals/repositorio/Repositorio.java
+++ src/main/java/edu/badpals/repositorio/Repositorio.java
@@ -4,7 +4,10 @@ package edu.badpals.repositorio;
 import edu.badpals.domain.MagicalItem;
 import edu.badpals.domain.Wizard;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.transaction.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -18,11 +21,50 @@ public class Repositorio {
 
     }
 
+
     public Optional<MagicalItem> loadItem(String name){
 
         Optional<MagicalItem> item = MagicalItem.find("name = ?1", name).firstResultOptional();
         return item;
     }
 
+    public Optional<MagicalItem> loadItem(MagicalItem magicalItem){
 
+        List<MagicalItem> items = MagicalItem.listAll();
+
+        MagicalItem itemFilter = null;
+
+        for (MagicalItem item : items){
+            if (item.getName().equals(magicalItem.getName()) && item.getQuality() == magicalItem.getQuality() && item.getType().equals(magicalItem.getType())){
+                itemFilter = item;
+            }
+
+        }
+
+        return Optional.ofNullable(itemFilter);
+
+    }
+
+
+
+
+
+
+
+/**
+    public List<MagicalItem> loadItems(String name){
+
+        List<MagicalItem> items = MagicalItem.listAll();
+        List<MagicalItem> filterItems = new ArrayList<>();
+
+        for (MagicalItem item: items){
+
+            if (item.getName().equals(name)){
+                filterItems.add(item);
+            }
+        }
+        return filterItems;
+
+    }
+*/
 }
