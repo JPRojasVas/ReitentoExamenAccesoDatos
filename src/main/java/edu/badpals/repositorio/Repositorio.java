8543ad622d9a3c5fcbe99d038d8b44b464fef91c
a23juanprv <Juanparovas3@gmail.com>
2024-06-12 09:51:52 +0200

    feat (Repositorio.java) : add method loadWizard

--- src/main/java/edu/badpals/repositorio/Repositorio.java
+++ src/main/java/edu/badpals/repositorio/Repositorio.java
@@ -1,11 +1,21 @@
 package edu.badpals.repositorio;
 
 
+import edu.badpals.domain.Wizard;
 import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.Optional;
+
 
 @ApplicationScoped
 public class Repositorio {
 
+    public Optional<Wizard> loadWizard(String name){
+
+        Optional<Wizard> wizard = Wizard.find("name = ?1", name).firstResultOptional();
+        return wizard;
+
+    }
 
 
 }
